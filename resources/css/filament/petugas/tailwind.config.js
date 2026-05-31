dbb4de4c173dcb6ef5d26bfbeaab5db918097d5e
haris3n <haris3n@gmail.com>
2026-06-01 01:16:33 +0700

    yok lagi lagi

---
+++ resources/css/filament/petugas/tailwind.config.js
@@ -0,0 +1,10 @@
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/Petugas/**/*.php',
+        './resources/views/filament/petugas/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+}
